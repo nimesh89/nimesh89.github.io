5f90917b6806ab3e5d6f4c3ab02b957d7d7fc27d
nimesh.pramuditha <nimesh.pramuditha@ALSG-G31A132-D>
2019-08-29 14:21:00 +0800

    browser compatibility added

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 (function () {
-    let vh = window.innerHeight * 0.01;
+    var vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     var timeout;
     window.addEventListener('resize', () => {
         if (timeout) return;
         timeout = setTimeout(function () {
-            let vh = window.innerHeight * 0.01;
+            vh = window.innerHeight * 0.01;
             document.documentElement.style.setProperty('--vh', '' + vh + 'px');
             timeout = undefined;
         }, 300);
